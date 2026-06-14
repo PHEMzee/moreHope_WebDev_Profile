ce0b7cda203440d144df671a0371d60c15455f3d
PHEMzee <okesanjo1996@gmail.com>
2026-06-14 07:21:09 +0100

    vercel: failed to lookup view "home" fixed

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
@@ -8,7 +13,8 @@ const PORT = process.env.PORT || 3000;
 app.disable("x-powered-by");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.locals.year = new Date().getFullYear();
 
